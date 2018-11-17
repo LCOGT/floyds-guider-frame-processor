8df7e39e225faba37ab9d3ad6af8fef726002ab9
Engineering Account <eng@floyds.ogg.lco.gtn>
2018-11-17 01:23:39 +0000

    Added package data to setup.py because it wasn't installing correctly.

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ setup(name='floyds-guider-frame-processor',
       version='0.0.2',
       packages=find_packages(),
       package_dir={'floyds_guider': 'floyds_guider'},
+      
       include_package_data=True,
+      package_data = {'floyds_guider': 'floyds_guider/templates/styles.css'},
       install_requires=['numpy', 'matplotlib', 'astropy', 'lcogt_logging', 'jinja2'],
       entry_points={'console_scripts': ['process-floyds-guider=floyds_guider.main:process_guider_frames']})
