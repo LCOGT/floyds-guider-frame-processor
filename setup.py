42d357cb4b4bce7142a9569a118c10ef9d70cca9
Curtis McCully <cmccully@lco.global>
2018-11-26 19:36:41 +0000

    Fixed installation of template files for jinja.

--- setup.py
+++ setup.py
@@ -15,9 +15,8 @@ from setuptools import setup, find_packages
 setup(name='floyds-guider-frame-processor',
       author=['Curtis McCully'],
       author_email=['cmccully@lco.global'],
-      version='0.0.2',
+      version='0.0.3',
+      zip_safe=False,
       packages=find_packages(),
-      package_dir={'floyds_guider': 'floyds_guider'},
-      include_package_data=True,
       install_requires=['numpy', 'matplotlib', 'astropy', 'lcogt_logging', 'jinja2'],
       entry_points={'console_scripts': ['process-floyds-guider=floyds_guider.main:process_guider_frames']})
